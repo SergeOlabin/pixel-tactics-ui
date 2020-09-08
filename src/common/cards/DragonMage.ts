4027d8454e7a8bc4e1950223fbbc4285849e08df
Serge <serge.olabin@gmail.com>
2020-09-08 12:36:19 +0300

    added few cards

---
+++ src/common/cards/DragonMage.ts
@@ -0,0 +1,46 @@
+import { IHero, ILeader, AttackTypes } from '../Types';
+import { Character } from '../Characters';
+
+export class DragonMageHero implements IHero {
+  public readonly type = 'DragonMage';
+  public readonly attack = 2;
+  public readonly health = 4;
+
+  public vanguardDescription = `
+    Attack: Defeat a hero in melee.
+  `
+  public flankDescription = `
+    Supporter has +3 attack strength.
+  `
+  public rearDescription = `
+    Ranged Attack
+  `
+  public orderDescription = `
+    Give a hero +5 attack strength until the end of this wave.
+  `
+
+  public vanguardPower() { }
+  public flankPower() { }
+  public rearPower() { }
+  public order() { }
+}
+
+export class DragonMageLeader implements ILeader {
+  public readonly name = 'Adjenna Callista';
+  public readonly attack = 3;
+  public readonly health = 17;
+  public readonly attackType = AttackTypes.Melee;
+
+  public readonly powerDescription = `
+    Your opponent cannot use the 'clear corpse' action.
+    (They can still clear corpses by other effects, like Orders and Attack Powers.)
+  `;
+
+  public power() {
+  }
+}
+
+export const DragonMage: Character = {
+  Hero: DragonMageHero,
+  Leader: DragonMageLeader,
+};
---
+++ src/common/cards/Knight.ts
@@ -0,0 +1,45 @@
+import { IHero, ILeader, AttackTypes } from '../Types';
+import { Character } from '../Characters';
+
+export class KnightHero implements IHero {
+  public readonly type = 'Knight';
+  public readonly attack = 3;
+  public readonly health = 10;
+
+  public vanguardDescription = `
+    Intercept. A hero who attacks this hero takes 2 damage.
+  `
+  public flankDescription = `
+    Your leader takes no damage from ranged attacks.
+  `
+  public rearDescription = `
+    All heroes in your unit can apply damage freely to this hero.
+  `
+  public orderDescription = `
+    Do 4 damage to every enemy hero or leader in melee.
+  `
+
+  public vanguardPower() { }
+  public flankPower() { }
+  public rearPower() { }
+  public order() { }
+}
+
+export class KnightLeader implements ILeader {
+  public readonly name = 'Cadenza';
+  public readonly attack = 4;
+  public readonly health = 20;
+  public readonly attackType = AttackTypes.Melee;
+
+  public readonly powerDescription = `
+    Your heroes and leader take 1 less damage from attacks.
+  `;
+
+  public power() {
+  }
+}
+
+export const Knight: Character = {
+  Hero: KnightHero,
+  Leader: KnightLeader,
+};
